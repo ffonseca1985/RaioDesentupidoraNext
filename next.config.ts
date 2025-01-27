1029adb157ff8d19163a88406c4868f9c3662562
Fabio Fonseca <ffonseca@jeracapital.com.br>
2025-01-26 22:48:35 -0300

    adjustments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    domains: ['d36p6ty9wanxdj.cloudfront.net'],
+    unoptimized: true,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/avif', 'image/webp']  
+  }
 };
 
 export default nextConfig;
